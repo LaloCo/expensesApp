d93b83d8f9f5cfede538aef825434156630f76d4
Eduardo Rosas <lalorosas@outlook.com>
2020-07-25 12:02:57 -0500

    Solve exception as reported by Siemen
    Manifest contained two Application tags
    
    file_provider_paths was missing the definition of the used folder
    
    The chooser intent requires a new flag when compiled for 28

--- expensessApp.Android/Dependencies/Share.cs
+++ expensessApp.Android/Dependencies/Share.cs
@@ -24,13 +24,14 @@ namespace expensessApp.Droid.Dependencies
         public Task Show(string title, string message, string filePath)
         {
             var intent = new Intent(Intent.ActionSend);
-            var documentUri = FileProvider.GetUriForFile(CrossCurrentActivity.Current.AppContext, "com.companyname.expensessapp.fileprovider", new Java.IO.File(filePath));
+            var documentUri = FileProvider.GetUriForFile(CrossCurrentActivity.Current.AppContext, "com.companyname.expensessapp.provider", new Java.IO.File(filePath));
             intent.PutExtra(Intent.ExtraStream, documentUri);
             intent.PutExtra(Intent.ExtraText, title);
             intent.PutExtra(Intent.ExtraSubject, message);
 
             var chooserIntent = Intent.CreateChooser(intent, title);
-            chooserIntent.SetFlags(ActivityFlags.GrantReadUriPermission);
+            chooserIntent.AddFlags(ActivityFlags.NewTask);
+            chooserIntent.AddFlags(ActivityFlags.GrantReadUriPermission);
             Android.App.Application.Context.StartActivity(chooserIntent);
             return Task.FromResult(true);
         }
